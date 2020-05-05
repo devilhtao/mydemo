ef7b2da7ea205a9ecc2289b0bb1c73a7ceed5cfc
devil_htao <devil_htao@163.com>
2020-05-05 11:51:17 +0800

    ssss

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -11,7 +11,7 @@ public class DemoApplicationTests {
 
     @Test
     public void contextLoads() {
-        System.out.println("====2023====");
+        System.out.println("====2024====");
     }
 
 }
